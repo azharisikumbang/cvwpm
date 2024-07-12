ccbad78832fc3b2877c52960044468f194ba3864
Kum <azharisaputrasikumbang@gmail.com>
2024-07-13 01:52:15 +0700

    fix: change button color

--- resources/views/admin-stock/toko/edit.blade.php
+++ resources/views/admin-stock/toko/edit.blade.php
@@ -71,7 +71,7 @@ route('admin-stock.toko.index') => 'Data Master Toko',
 
             <div>
                 <button type="submit"
-                    class="w-full text-white bg-orange-600 hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Simpan</button>
+                    class="w-full text-white bg-orange-500 hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Simpan</button>
             </div>
         </form>
     </div>
