2b9b016a8c71b2be03cda53825346bdb06e66d8e
Kum <azharisaputrasikumbang@gmail.com>
2024-07-13 00:12:17 +0700

    make email nullable and add kontak field

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,10 +14,11 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
             $table->string('username')->unique();
-            $table->string('email')->unique();
+            $table->string('email')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('role_id')->index();
+            $table->string('kontak')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
