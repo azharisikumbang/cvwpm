61c7fb58fc70f7ca07f83b22cfc6aa756de787a5
Kum <azharisaputrasikumbang@gmail.com>
2024-07-12 23:38:01 +0700

    add method for acting as role

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,55 @@
 
 namespace Tests;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function asAdminWeb()
+    {
+        $user = User::factory()->create([
+            'role_id' => Role::ID_ADMIN_WEB
+        ]);
+
+        $this->actingAs($user);
+
+        return $user;
+    }
+    public function asAdminStock()
+    {
+        $user = User::factory()->create([
+            'role_id' => Role::ID_ADMIN_STOCK
+        ]);
+
+        $this->actingAs($user);
+
+        return $user;
+    }
+    public function asAdminPurchasing()
+    {
+        $user = User::factory()->create([
+            'role_id' => Role::ID_ADMIN_PURCHASING
+        ]);
+
+        $this->actingAs($user);
+
+        return $user;
+    }
+    public function asSales()
+    {
+        $user = User::factory()->create([
+            'role_id' => Role::ID_SALES
+        ]);
+
+        $this->actingAs($user);
+
+        return $user;
+    }
+
+    public function asGuest()
+    {
+        $this->assertGuest();
+    }
 }
