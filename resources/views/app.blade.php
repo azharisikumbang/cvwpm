aa1d396db7e7f95a7adef8b6d72029169c33e793
Kum <azharisaputrasikumbang@gmail.com>
2024-07-21 01:11:45 +0700

    fix: add this line when tried to load on ngrok

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') - {{ env('APP_NAME') }}</title>
 
@@ -27,9 +28,15 @@
 
     </div>
 
-    <div class="bg-red-200 bg-green-200 bg-yellow-200" style="display:none"></div>
-    @yield('script')
+    <div class="
+        bg-red-100 bg-green-100 bg-yellow-100
+        bg-red-200 bg-green-200 bg-yellow-200
 
+        
+        text-red-100 text-green-100 text-yellow-100
+        text-red-200 text-green-200 text-yellow-200
+    " style="display:none"></div>
+    @yield('script')
 </body>
 
 </html>
