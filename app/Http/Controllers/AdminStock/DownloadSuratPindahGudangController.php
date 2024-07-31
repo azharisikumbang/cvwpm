f35795df39091d6d3d55726ae2ed5ab4cde52fae
Kum <azharisaputrasikumbang@gmail.com>
2024-07-31 21:18:06 +0700

    feat: download file surat jalan pindah gudang dalam pdf

--- app/Http/Controllers/AdminStock/DownloadSuratPindahGudangController.php
+++ app/Http/Controllers/AdminStock/DownloadSuratPindahGudangController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\AdminStock;
 
 use App\Http\Controllers\Controller;
+use App\Models\PindahGudang;
 use Illuminate\Http\Request;
 
 class DownloadSuratPindahGudangController extends Controller
@@ -10,8 +11,16 @@ class DownloadSuratPindahGudangController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(PindahGudang $pindahGudang)
     {
-        //
+        abort_if(
+            $pindahGudang->surat_jalan_file === null
+            || !file_exists(storage_path('app/public/surat-jalan-pindah-gudang/' . $pindahGudang->surat_jalan_file)),
+            404
+        );
+
+        return response()->file(
+            storage_path("app/public/surat-jalan-pindah-gudang/{$pindahGudang->surat_jalan_file}")
+        );
     }
 }
