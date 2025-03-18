e0f5e3d24d02fb76600de6efcf6c5978e53141cc
Kum <azharisaputrasikumbang@gmail.com>
2025-03-18 21:48:10 +0700

    add welcome message for authenticated user

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -3,7 +3,14 @@
 @section('title', 'Page Title')
 
 @section('content')
-<p>This is my body content.</p>
+<div>
+    <h2 class="text-xl">Selamat Datang</h2>
+    <p class="mb-2">
+        Halo, {{ auth()->user()->staf->nama }} ! Kamu masuk sebagai {{ auth()->user()->jabatan }} di {{
+        auth()->user()->staf->gudangKerja->nama }}.
+    </p>
+    <p>Sekarang : {{ format_tanggal_indonesia(now()) . ' ' . date('H:i T') }}</p>
+</div>
 @endsection
 
 @section('script')
